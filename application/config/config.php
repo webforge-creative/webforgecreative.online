f8635fd73272dd9296451fcfa66b1b4fad2e0cbe
Abdullah <abdullahriaz8161@gmail.com>
2024-09-17 18:58:42 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webforgecreative.online/';
+$config['base_url'] = 'https://www.webforgecreative.online/';
 
 /*
 |--------------------------------------------------------------------------
