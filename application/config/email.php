7b6c0aa83ce247456f87e3decdc127334cb02b6a
Abdullah <abdullahriaz8161@gmail.com>
2024-09-18 00:00:15 +0500

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,15 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-// SMTP Configuration for GMass
+// application/config/email.php
+
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'smtp.gmass.co';
-$config['smtp_port'] = 465;  // Use 465 for SSL or 587 for TLS
-$config['smtp_user'] = 'webforgecreative@gmail.com'; // Your Gmail address
-$config['smtp_pass'] = 'dxlb pmhz igcg aidk'; // Your Gmail password or app password
-$config['smtp_crypto'] = 'ssl';  // Use 'ssl' for port 465, 'tls' for port 587
-$config['mailtype'] = 'html';  // You can set this to 'text' if you don't want HTML emails
-$config['charset']  = 'utf-8';
+$config['smtp_port'] = 587;
+$config['smtp_user'] = 'gmass'; // Your Gmail address
+$config['smtp_pass'] = '57e492f3-d4ce-4b51-a74b-a534018c4882'; // Your Gmail password or app password
+$config['smtp_crypto'] = 'tls'; // or 'ssl' for SSL encryption
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['newline'] = "\r\n";
 $config['wordwrap'] = TRUE;
-$config['newline']  = "\r\n";  // Newline for proper formatting
-$config['smtp_timeout'] = 30;  // Optional: timeout for the SMTP request
